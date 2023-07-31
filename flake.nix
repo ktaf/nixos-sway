746a200cd9c66a2b31aa292defc714ba1f449743
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-07-31 23:36:35 +0300

    Remove formatter

--- flake.nix
+++ flake.nix
@@ -3,18 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    # used for development
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = inputs@{ self, nixpkgs, home-manager, treefmt-nix, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       user = "kourosh";
       system = "x86_64-linux";
@@ -39,37 +34,6 @@
               home-manager.users.${user} = import ./modules/home.nix;
             }
           ];
-        };
-      };
-      flake-parts.lib.mkFlake = {
-        inherit inputs;
-      } {
-        systems = [ "x86_64-linux" ];
-        imports = [
-          inputs.flake-root.flakeModule
-          inputs.mission-control.flakeModule
-          inputs.treefmt-nix.flakeModule
-        ];
-        perSystem = { config, ... }: {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs.mdsh.enable = true;
-            programs.nixpkgs-fmt.enable = true;
-            programs.shellcheck.enable = true;
-            programs.shfmt.enable = true;
-            programs.prettier.enable = true;
-            settings.formatter.prettier.options = [ "--prose-wrap" "always" ];
-            settings.formatter.shellcheck.options = [ "-s" "bash" ];
-            programs.beautysh = {
-              enable = true;
-              indent_size = 4;
-            };
-            # Here you can specify the formatters to use
-            programs.terraform.enable = true;
-            # ...and options
-            programs.terraform.package = nixpkgs.terraform_1;
-          };
-          formatter = config.treefmt.build.wrapper;
         };
       };
     };
