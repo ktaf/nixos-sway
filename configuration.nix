27492d3eda0ec31b9370fd5f6fa127c8d50b51d8
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-10-30 17:29:48 +0200

    refactor: aggregating services

--- configuration.nix
+++ configuration.nix
@@ -18,11 +18,16 @@
       auto-optimise-store = true;
       warn-dirty = false; # remove git warnings
     };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
   };
   environment.systemPackages = with pkgs; [
     vim
     wget
     git
+    # linuxKernel.packages.linux_6_5.nvidia_x11_beta
   ];
 
   #fonts
@@ -36,14 +41,21 @@
     fira-code-symbols
     jetbrains-mono
   ];
-  #emojis
-  services.gollum.emoji = true;
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.kernelPackages = pkgs.linuxPackages_6_5; # pkgs.linuxPackages_latest
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 5;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+    };
+    kernelPackages = pkgs.linuxPackages_6_5; # pkgs.linuxPackages_latest
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Tallinn";
@@ -52,52 +64,23 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-
-  };
-
-  services.xserver.displayManager.lightdm.enable = false;
-
   #swaylock pass verify
-  security.pam.services.swaylock = { };
-
-  #Flatpak
-  services.flatpak.enable = true;
-  #locate
-  # services.locate.enable = true;
-
-  # Enable sound with pipewire.
-  sound = {
-    enable = true;
-    mediaKeys.enable = true;
+  security = {
+    pam.services.swaylock = { };
+    rtkit.enable = true;
   };
-  hardware.pulseaudio = {
-    enable = false;
-    extraModules = [ pkgs.pulseaudio-modules-bt ];
-  };
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    #isDefault
-    wireplumber.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  hardware = {
+    pulseaudio = {
+      enable = false;
+      extraModules = [ pkgs.pulseaudio-modules-bt ];
+    };
+    bluetooth = { enable = true; };
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  services.dbus.enable = true;
   # XDG Configuration
   xdg.portal = {
     enable = true;
@@ -109,51 +92,86 @@
     wlr.enable = true;
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable the Bluethooth daemon.
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  services.thermald.enable = true;
-
-  #tlp
-  services.tlp = {
+  # Enable sound
+  sound = {
     enable = true;
-    settings = {
-      PCIE_ASPM_ON_BAT = "powersupersave";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_MAX_PERF_ON_AC = "100";
-      CPU_MAX_PERF_ON_BAT = "60";
-      STOP_CHARGE_THRESH_BAT1 = "95";
-      STOP_CHARGE_THRESH_BAT0 = "95";
+    mediaKeys.enable = true;
+  };
+
+  # List services that you want to enable:
+  services = {
+    #emojis
+    gollum.emoji = true;
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+      displayManager.lightdm.enable = false;
+    };
+    #Flatpak
+    flatpak.enable = true;
+    #locate
+    # locate.enable = true;
+
+    # Enable Pipewire
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      #isDefault
+      wireplumber.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+
+    # Enable the Bluethooth daemon.
+    blueman.enable = true;
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    xserver.libinput.enable = true;
+
+    dbus.enable = true;
+
+    #tlp
+    tlp = {
+      enable = true;
+      settings = {
+        PCIE_ASPM_ON_BAT = "powersupersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_MAX_PERF_ON_AC = "100";
+        CPU_MAX_PERF_ON_BAT = "60";
+        STOP_CHARGE_THRESH_BAT1 = "95";
+        STOP_CHARGE_THRESH_BAT0 = "95";
+      };
+    };
+
+    #auto-cpufreq
+    auto-cpufreq.enable = true;
+
+    #upower dbus
+    upower.enable = true;
+
+    # Solve AT-SPI error
+    gnome.at-spi2-core.enable = true;
+
+    # Enable Firmware manager
+    fwupd = {
+      enable = true;
+      package = pkgs.fwupd;
     };
   };
-
-  #auto-cpufreq
-  services.auto-cpufreq.enable = true;
-
-  #upower dbus
-  services.upower.enable = true;
-
-  # Enable Firmware manager
-  services.fwupd = {
-    enable = true;
-    package = pkgs.fwupd;
-  };
-
-  # Solve AT-SPI error
-  services.gnome.at-spi2-core.enable = true;
 
   # This is needed for FortinetSSL VPN 
   environment.etc."ppp/options".text = "ipcp-accept-remote";
