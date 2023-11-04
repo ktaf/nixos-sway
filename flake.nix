fbf83605e652295e56845390518f28c5149af9f7
Kourosh Tafreshi <kourosh.tafreshi@coolbet.com>
2023-11-04 12:38:42 +0200

    fmt: re-format flake.nix

--- flake.nix
+++ flake.nix
@@ -3,20 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:ktaf/nixos-hardware";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, nixos-hardware, ... }:
+  outputs = { nixpkgs, home-manager, nixos-hardware, ... }@inputs:
     let
       user = "kourosh";
       system = "x86_64-linux";
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
-        ${user} = nixpkgs.lib.nixosSystem {
+        xps9510 = nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = { inherit user; };
           modules = [
@@ -26,10 +26,12 @@
             nixos-hardware.nixosModules.dell-xps-15-9510-nvidia
             home-manager.nixosModules.home-manager
             {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit user; };
-              home-manager.users.${user} = import ./modules/home.nix;
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit user; };
+                users.${user} = import ./modules/home.nix;
+              };
             }
           ];
         };
